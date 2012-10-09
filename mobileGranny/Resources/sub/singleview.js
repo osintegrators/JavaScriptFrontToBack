e720aa79f81819e8eb7a703c7f7749a149807457
Jonathan Freeman <jfreeman@osintegrators.com>
2012-10-08 22:45:10 -0500

    Making text darker for the old farts...

--- mobileGranny/Resources/sub/singleview.js
+++ mobileGranny/Resources/sub/singleview.js
@@ -4,139 +4,141 @@ var editMode = false;
 var singleInfo;
 
 // function to assign values to the labels
-var hydrate = function(data){
-    singleInfo = data;
+var hydrate = function(data) {
+	singleInfo = data;
 
-    id = data._id;
-    name = data.name;
-    phone = data.phone;
-    address = data.address;
-    email = data.email;
-    
-    nameLabel.text = name;
-    phoneLabel.text = phone;
-    addressLabel.text = address;
-    emailLabel.text = email;
+	id = data._id;
+	name = data.name;
+	phone = data.phone;
+	address = data.address;
+	email = data.email;
 
-    nameText.value= name;
-    phoneText.value = phone;
-    addressText.value = address;
-    emailText.value = email;
-};
+	nameLabel.text = name;
+	phoneLabel.text = phone;
+	addressLabel.text = address;
+	emailLabel.text = email;
 
-    
-var view = Ti.UI.createView({
-    top: 0,
-    bottom: 0
-});
+	nameText.value = name;
+	phoneText.value = phone;
+	addressText.value = address;
+	emailText.value = email;
+	};
 
-//viewView
-var nameLabel = Ti.UI.createLabel({
-font: {
-    fontSize:'18dp',
-    fontWeight:'bold'
-},
-top:'20dp'
-});
+	var view = Ti.UI.createView({
+		top : 0,
+		bottom : 0
+	});
 
-var phoneLabel = Ti.UI.createLabel({
-top: '68dp',
-left: '48dp'
-});
+	//viewView
+	var nameLabel = Ti.UI.createLabel({
+		color : 'black',
+		font : {
+			fontSize : '18dp',
+			fontWeight : 'bold'
+
+		},
+		top : '20dp'
+	});
+
+	var phoneLabel = Ti.UI.createLabel({
+		color : 'black',
+		top : '68dp',
+		left : '48dp'
+	}); 
+
 
 var addressLabel = Ti.UI.createLabel({
-top: '112dp',
-left: '48dp'
+	color : 'black',
+	top : '112dp',
+	left : '48dp'
 });
 
 var emailLabel = Ti.UI.createLabel({
-top: '160dp',
-left: '48dp'
+	color : 'black',
+	top : '160dp',
+	left : '48dp'
 });
 
 //editView
 var nameText = Ti.UI.createTextField({
-    hintText:'Full Name',
-    keyboardType: Titanium.UI.KEYBOARD_URL,
-    top:'20dp',
-    left:'48dp',
-    visible:false
+	hintText : 'Full Name',
+	keyboardType : Titanium.UI.KEYBOARD_URL,
+	top : '20dp',
+	left : '48dp',
+	visible : false
 });
 
 var phoneText = Ti.UI.createTextField({
-    hintText:'Phone Number',
-    keyboardType: Titanium.UI.KEYBOARD_PHONE_PAD,
-    top: '68dp',
-    left: '48dp',
-    visible:false
+	hintText : 'Phone Number',
+	keyboardType : Titanium.UI.KEYBOARD_PHONE_PAD,
+	top : '68dp',
+	left : '48dp',
+	visible : false
 });
 
 var addressText = Ti.UI.createTextField({
-    hintText:'Home Address',
-    keyboardType: Titanium.UI.KEYBOARD_URL,
-    top: '112dp',
-    left: '48dp',
-    visible:false
+	hintText : 'Home Address',
+	keyboardType : Titanium.UI.KEYBOARD_URL,
+	top : '112dp',
+	left : '48dp',
+	visible : false
 });
 
 var emailText = Ti.UI.createTextField({
-    hintText:'Email Address',
-    keyboardType: Titanium.UI.KEYBOARD_URL,
-    top: '160dp',
-    left: '48dp',
-    visible:false
+	hintText : 'Email Address',
+	keyboardType : Titanium.UI.KEYBOARD_URL,
+	top : '160dp',
+	left : '48dp',
+	visible : false
 });
-
 
 //create buttons
 var saveButton = Ti.UI.createButton({
-    title: 'Save changes',
-    top:'400dp',
-    right:'48dp',
-    width: '92dp',
-    visible: false
+	title : 'Save changes',
+	top : '400dp',
+	right : '48dp',
+	width : '92dp',
+	visible : false
 });
 
 var editCancel = Ti.UI.createButton({
-    title: 'Edit Entry',
-    top:'400dp',
-    left:'48dp',
-    width:'92dp'
+	title : 'Edit Entry',
+	top : '400dp',
+	left : '48dp',
+	width : '92dp'
 });
 var backButton = Ti.UI.createButton({
-    title:'Close'
+	title : 'Close'
 });
 singleView.setLeftNavButton(backButton);
 
 //add event listeners
-editCancel.addEventListener('click', function(){
-    if (editMode == false){
-        editMode = true;
-        Functions.makeEditable(view,id);
-    }
-    else{
-        editMode = false;
-        Functions.resetEdits(view,singleInfo);
-    }
+editCancel.addEventListener('click', function() {
+	if (editMode == false) {
+		editMode = true;
+		Functions.makeEditable(view, id);
+	} else {
+		editMode = false;
+		Functions.resetEdits(view, singleInfo);
+	}
 });
 
-Ti.App.addEventListener('changesSaved', function(data){
-   singleInfo = data;
-    
-    if(typeof data._id === 'undefined') {
-    	singleView.close();
-    }
-    else {
-	    editCancel.fireEvent('click');
-	    Ti.App.fireEvent('updateList');
-    }
+Ti.App.addEventListener('changesSaved', function(data) {
+	singleInfo = data;
+
+	if ( typeof data._id === 'undefined') {
+		singleView.close();
+	} else {
+		editCancel.fireEvent('click');
+		Ti.App.fireEvent('updateList');
+	}
 });
-saveButton.addEventListener('click', function(){
-    var updatedPerson = Functions.makeUpdatedPerson(view);
-    Functions.setAddress(id,updatedPerson);
+saveButton.addEventListener('click', function() {
+	var updatedPerson = Functions.makeUpdatedPerson(view);
+	Functions.setAddress(id, updatedPerson);
 });
-view.addEventListener('click', function(){
-    Ti.UI.Android.hideSoftKeyboard();
+view.addEventListener('click', function() {
+	Ti.UI.Android.hideSoftKeyboard();
 });
 
 //add labels to view
@@ -150,34 +152,33 @@ view.add(nameText);
 view.add(addressText);
 view.add(phoneText);
 view.add(emailText);
- 
+
 // hydrate the fields
-if (singleView.singleId == 'new'){
-    var id = 'new';
-    var name = '';
-    var phone = '';
-    var address = '';
-    var email = '';
-    // TODO don't know if things need to be reworked here
-}
-else{
-    Functions.getAddress(singleView.singleId, hydrate);
+if (singleView.singleId == 'new') {
+	var id = 'new';
+	var name = '';
+	var phone = '';
+	var address = '';
+	var email = '';
+	// TODO don't know if things need to be reworked here
+} else {
+	Functions.getAddress(singleView.singleId, hydrate);
 }
 
 //add buttons to view
-view.add(editCancel);    
+view.add(editCancel);
 view.add(saveButton);
 
-if (singleView.singleId == 'new'){
-    editCancel.fireEvent('click');
+if (singleView.singleId == 'new') {
+	editCancel.fireEvent('click');
 }
 
 //add views to scrollview
 var contactView = Ti.UI.createScrollView({
-    contentWidth:'auto',
-    contentHeight:'auto',
-    showHorizontalScrollIndicator:false,
-    showVerticalScrollIndicator:true
+	contentWidth : 'auto',
+	contentHeight : 'auto',
+	showHorizontalScrollIndicator : false,
+	showVerticalScrollIndicator : true
 });
 contactView.add(view);
 
